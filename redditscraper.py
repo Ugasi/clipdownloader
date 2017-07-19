e0d50d127b53fd6333829c5110a3f8f8dfb966e8
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-07-19 16:55:08 +0300

    get twitch clip urls and print them to console

--- redditscraper.py
+++ redditscraper.py
@@ -4,6 +4,8 @@ Scrapes specified subreddit for clips.twitch.tv urls
 
 from selenium import webdriver
 
+TWITCH_CLIP_XPATH = "//div[@class='top-matter']//a[contains(@data-href-url,'clips.twitch')]"
+
 def scrape_sub(url):
     """
     Get twitch clip urls from specified subreddit
@@ -12,3 +14,8 @@ def scrape_sub(url):
     #driver = webdriver.PhantomJS()
     driver = webdriver.Firefox()
     driver.get(url)
+    elements = driver.find_elements_by_xpath(TWITCH_CLIP_XPATH)
+    for element in elements:
+        print(element.get_attribute("data-href-url"))
+
+scrape_sub("https://www.reddit.com/r/hearthstone/")
