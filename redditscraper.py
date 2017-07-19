7a79b052ad7137501f18fb1bf27d06828569638b
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-07-19 17:04:39 +0300

    renamed function and made it return list of urls

--- redditscraper.py
+++ redditscraper.py
@@ -6,16 +6,17 @@ from selenium import webdriver
 
 TWITCH_CLIP_XPATH = "//div[@class='top-matter']//a[contains(@data-href-url,'clips.twitch')]"
 
-def scrape_sub(url):
+def scrape_twitch_links(url):
     """
     Get twitch clip urls from specified subreddit
     """
 
-    #driver = webdriver.PhantomJS()
-    driver = webdriver.Firefox()
+    driver = webdriver.PhantomJS()
+    #driver = webdriver.Firefox()
     driver.get(url)
     elements = driver.find_elements_by_xpath(TWITCH_CLIP_XPATH)
+    urls = []
     for element in elements:
-        print(element.get_attribute("data-href-url"))
-
-scrape_sub("https://www.reddit.com/r/hearthstone/")
+        value = element.get_attribute("data-href-url")
+        urls.append(value)
+    return urls
