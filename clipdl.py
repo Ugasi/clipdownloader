ee6a7f6b19f0050b34909b7e2a4c24a696a3727d
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-07-19 15:07:14 +0300

    more describing function description and default name

--- clipdl.py
+++ clipdl.py
@@ -4,9 +4,11 @@ Twitch clip downloader
 
 import requests
 
-def download_clip(url, name):
+def download_clip(url, name="video"):
     """
-    Downloads twitch clip from given url
+    Downloads twitch clip from given url.
+    Saves video as mp4.
+    Default filename is 'video'
     """
     res = requests.get(url)
     res.raise_for_status()
