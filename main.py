d2d741a8f72340be1507a205551976afe0e3002a
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-07-25 15:31:16 +0300

    now uses reddit's search function to get clips from today

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ Runs everything
 import clipdl
 import redditscraper
 
+SUBREDDIT = "https://www.reddit.com/r/hearthstone/"
+SEARCH = "search?q=clips.twitch.tv&restrict_sr=on&sort=hot&t=day"
+
 def main():
     """
     Main
     """
-    video_urls = redditscraper.scrape_twitch_links("https://www.reddit.com/r/hearthstone/")
+    url = SUBREDDIT + SEARCH
+    video_urls = redditscraper.scrape_twitch_links(url)
     for cur, video_url in enumerate(video_urls):
         clipdl.download_clip(video_url, str(cur))
 
--- redditscraper.py
+++ redditscraper.py
@@ -3,12 +3,13 @@ Scrapes specified subreddit for clips.twitch.tv urls
 """
 
 from selenium import webdriver
+import requests
 
-TWITCH_CLIP_XPATH = "//div[@class='top-matter']//a[contains(@data-href-url,'clips.twitch')]"
+TWITCH_CLIP_XPATH = "//div[@class='search-result-footer']//a[contains(@href,'clips.twitch')]"
 
 def scrape_twitch_links(url):
     """
-    Get twitch clip urls from specified subreddit
+    Get twitch clip urls from specified subreddit's search results
     """
 
     #driver = webdriver.PhantomJS()
@@ -17,7 +18,7 @@ def scrape_twitch_links(url):
     elements = driver.find_elements_by_xpath(TWITCH_CLIP_XPATH)
     urls = []
     for element in elements:
-        value = element.get_attribute("data-href-url")
+        value = element.get_attribute("href")
         urls.append(value)
 
     video_urls = []
