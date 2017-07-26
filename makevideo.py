1cd05637073dde8dfe9efb539721ba9576ec4cf8
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-07-26 22:44:47 +0300

    :(

--- makevideo.py
+++ makevideo.py
@@ -2,5 +2,9 @@
 Script for making videos in Blender.
 Can only be run in Blender because of bpy import
 """
-
-print("testi")
+#import bpy
+import os
+CLIP_PATH = "videos\\"
+CLIPS = []
+for clip in os.listdir(CLIP_PATH):
+    CLIPS.append(CLIP_PATH+clip)
