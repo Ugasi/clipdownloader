1a7134bbb56ba4283a0c34b372ce5bc862a5c392
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-08-01 05:53:24 +0300

    audio fade in and out

--- makevideo.py
+++ makevideo.py
@@ -3,10 +3,10 @@ Moviepy script
 Combines all videos into single video
 """
 from moviepy.editor import VideoFileClip as vfc, concatenate_videoclips as cvc, ColorClip as cc
-from moviepy.video.fx.fadein import fadein
-from moviepy.video.fx.fadeout import fadeout
-
-CLIP_PATH = "videos\\"
+from moviepy.video.fx.fadein import fadein as fi
+from moviepy.video.fx.fadeout import fadeout as fo
+from moviepy.audio.fx.audio_fadein import audio_fadein as afi
+from moviepy.audio.fx.audio_fadeout import audio_fadeout as afo
 
 def make_video(video_paths):
     """
@@ -14,7 +14,7 @@ def make_video(video_paths):
     """
     videos = []
     for path in video_paths:
-        video = fadein(fadeout(vfc(path, target_resolution=(1080, 1920)), 1), 1)
+        video = afi(afo(fi(fo(vfc(path, target_resolution=(1080, 1920)), 1), 1), 1), 1)
         videos.append(video)
 
     black_image = cc((1920, 1080), color=[0, 0, 0], duration=0.5)
