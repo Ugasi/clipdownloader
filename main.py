f30f5f294e75f6498d26be670af82e1f963bdff4
Juha Palmu <juhamikaelpalmu@gmail.com>
2017-08-03 02:03:23 +0300

    script runnable multiple times. ignores duplicate clips. this allows for more videos to be collected before making final video

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ def main():
         subreddit = re.search(PARSE_SUB, url)
         subreddit = subreddit.group("subreddit")
         save_path = os.path.join(CLIP_PATH, subreddit)
-        clips = rs.get_twitch_info(rs.scrape_twitch_links(url))
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        clips = rs.get_twitch_info(rs.scrape_twitch_links(url), save_path)
         for index, clip in enumerate(clips):
             clip.location = save_path
             clipdl.download_clip(clip)
             print("Video: "+str(index))
-            time.sleep(2)
+            time.sleep(1)
         video_paths = []
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
         for clip in os.listdir(save_path):
             video_paths.append(os.path.join(save_path, clip))
         mv.make_video(video_paths)
--- redditscraper.py
+++ redditscraper.py
@@ -2,21 +2,24 @@
 Scrapes specified subreddit for clips.twitch.tv urls
 """
 import re
+import os
 import requests
 from bs4 import BeautifulSoup as bs
 from clip import Clip
 
 TWITCH_BS_STRING = "div[class='search-result-footer'] > a[href*=clips.twitch]"
 VID_SOURCE_BS_STRING = "script"
-STREAMER_BS_STRING = "div[class='view-bc-meta__name ellipsis'] > a"
-CLIP_NAME_BS_STRING = "div[class='view-clip__title]"
+STREAMER_BS_STRING = "div[class='view-bc-meta__name ellipsis']"
+CLIP_NAME_BS_STRING = "title"
 REGEX = r"(?P<url>https://clips-media-assets\.twitch\.tv.*?offset.*?mp4)"
 SRC_ATTR = "src"
 HREF_ATTR = "href"
+INVALID = r"[^a-zA-Z0-9]"
 AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0"
 HEADERS = {
     'User-Agent':AGENT
 }
+
 def scrape_twitch_links(url):
     """
     Get twitch clip urls from specified subreddit's search results
@@ -27,14 +30,17 @@ def scrape_twitch_links(url):
     urls = []
     for link in links:
         urls.append(link.get(HREF_ATTR))
-    print("Amount of videos expected: "+str(len(links)))
-    print(urls)
+    print("Videos expected: "+str(len(links)))
     return urls
 
-def get_twitch_info(urls):
+def get_twitch_info(urls, save_path):
     """
     Parses video source, streamer name and clip name from given list of twitch url
     """
+    current_vids = []
+    for clip in os.listdir(save_path):
+        current_vids.append(clip)
+
     clips = []
     for index, url in enumerate(urls):
         vid_src = None
@@ -47,13 +53,17 @@ def get_twitch_info(urls):
             if matches is not None:
                 vid_src = matches.group("url")
                 break
-        if "index" in vid_src:
-            print("Skipping video")
-            continue
-        else:
-            print("Video from "+vid_src)
+        #if "index" in vid_src:
+        #    print("Skipping video")
+        #    continue
+        #else:
         streamer = soup.select(STREAMER_BS_STRING)
-        clip_name = str(index)
+        clip_name = soup.select_one(CLIP_NAME_BS_STRING).text
+        clip_name = re.sub(INVALID, "", clip_name)
+        if clip_name+Clip.file_format in current_vids:
+            print(str(index)+": Already got this. Skipping")
+            continue
+        print(str(index)+": Video from "+vid_src)
         clip = Clip(vid_src, streamer, clip_name, None)
         clips.append(clip)
 
