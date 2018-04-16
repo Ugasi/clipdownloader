a74d1f6f7d29d0c98899e720a803ef21cd5b83f6
Juha Palmu <juhamikaelpalmu@gmail.com>
2018-04-17 01:00:03 +0300

    Refactor makevideo.py to be more readable

--- makevideo.py
+++ makevideo.py
@@ -2,11 +2,11 @@
 Moviepy script
 Combines all videos into single video
 """
-from moviepy.editor import VideoFileClip as vfc, concatenate_videoclips as cvc, ColorClip as cc
-from moviepy.video.fx.fadein import fadein as fi
-from moviepy.video.fx.fadeout import fadeout as fo
-from moviepy.audio.fx.audio_fadein import audio_fadein as afi
-from moviepy.audio.fx.audio_fadeout import audio_fadeout as afo
+from moviepy.editor import VideoFileClip, concatenate_videoclips as concat, ColorClip
+from moviepy.video.fx.fadein import fadein
+from moviepy.video.fx.fadeout import fadeout
+from moviepy.audio.fx.audio_fadein import audio_fadein
+from moviepy.audio.fx.audio_fadeout import audio_fadeout
 
 def make_video(video_paths):
     """
@@ -14,9 +14,13 @@ def make_video(video_paths):
     """
     videos = []
     for path in video_paths:
-        video = afi(afo(fi(fo(vfc(path, target_resolution=(1080, 1920)), 1), 1), 1), 1)
+        video = audio_fadein(
+            audio_fadeout(
+                fadein(
+                    fadeout(
+                        VideoFileClip(path, target_resolution=(1080, 1920)), 1), 1), 1), 1)
         videos.append(video)
 
-    black_image = cc((1920, 1080), color=[0, 0, 0], duration=0.5)
-    result_clip = cvc(videos, transition=black_image, method="compose")
+    black_image = ColorClip((1920, 1080), color=[0, 0, 0], duration=0.5)
+    result_clip = concat(videos, transition=black_image, method="compose")
     result_clip.write_videofile("result.mp4", fps=60, preset="ultrafast")
